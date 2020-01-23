9a3c2e479972030d5fe21a3d1ee2ff72e0ddf78a
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-22 21:42:51 -0500

    global filter and chaining them

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.filter('to-lowercase', function(v) {
+  return v.toLowerCase();
+});
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
