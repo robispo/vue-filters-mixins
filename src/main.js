1daf9d41293c00c6b4a420e47784a8612ad318ef
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-22 22:22:51 -0500

    Global mixin

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ Vue.filter('toLowerCase', function(v) {
   return v.toLowerCase();
 });
 
+Vue.mixin({
+  created() {
+    console.log('Global-mixin!');
+    
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
